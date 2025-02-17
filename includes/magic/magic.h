9b96c093d467ea3245ae6fd444075f1d7c6c476d
Viktor95801 <155126585+Viktor95801@users.noreply.github.com>
2025-02-17 12:40:14 -0300

    Atualizar o magic.h
    Added "extern" keyword for small fix

--- includes/magic/magic.h
+++ includes/magic/magic.h
@@ -3,22 +3,22 @@
 
 #include <stdint.h>
 
-uint64_t cacheBishopAtk[64][512]; // 256 K
-uint64_t cacheRookAtk[64][4096];   // 2048K
+extern uint64_t cacheBishopAtk[64][512]; // 256 K
+extern uint64_t cacheRookAtk[64][4096];   // 2048K
 
-uint64_t bishopMask[64];
-uint64_t rookMask[64];
+extern uint64_t bishopMask[64];
+extern uint64_t rookMask[64];
 
 /* typedef struct {
    uint64_t mask;  // to mask relevant squares of both lines (no outer squares)
    uint64_t magic; // magic 64-bit factor
 } SMagic;
 
-SMagic cacheBishopTbl[64];
-SMagic cacheRookTbl[64]; */
+extern SMagic cacheBishopTbl[64];
+extern SMagic cacheRookTbl[64]; */
 
-uint64_t rook_magics[64];
-uint64_t bishop_magics[64];
+extern uint64_t rook_magics[64];
+extern uint64_t bishop_magics[64];
 
 void initBishopAtk_cache();
 void initRookAtk_cache();
